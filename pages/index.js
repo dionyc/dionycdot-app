37c9344ca18554bab03707f0d0ea5b78209a3bfb
Diony Concepcion <Diony.Concepcion@ibm.com>
2022-02-14 10:13:36 -0500

    adding google analytics

--- pages/index.js
+++ pages/index.js
@@ -2,16 +2,9 @@ import Head from 'next/head'
 import Header from '@components/Header'
 import Footer from '@components/Footer'
 import Body from '@components/Body'
-import ReactGA from "react-ga";  //Google Analytics
-import { useEffect } from 'react' 
 
 export default function Home() {
-
-  useEffect(() => {
-    ReactGA.initialize('G-8PQGNVYQK6');
-    ReactGA.pageview(window.location.pathname);
-  })
-
+  
   return (
     <div className="container">
       <Head>
